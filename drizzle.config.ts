39f77d34687a3152a54a947dfc27932bbde8f183
Adarsh Chaudhary <chaudharyadarsh9919@gmail.com>
2026-09-09 22:56:55 +0530

    fix type error for root files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,15 @@
-import 'dotenv/config'
-import { defineConfig } from 'drizzle-kit'
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
-if (!process.env['DATABASE_URL_UNPOOLED']) {
-  throw new Error('DATABASE_URL_UNPOOLED is not set in the .env file')
+if (!process.env["DATABASE_URL_UNPOOLED"]) {
+  throw new Error("DATABASE_URL_UNPOOLED is not set in the .env file");
 }
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env['DATABASE_URL_UNPOOLED'],
+    url: process.env["DATABASE_URL_UNPOOLED"],
   },
-})
+});
